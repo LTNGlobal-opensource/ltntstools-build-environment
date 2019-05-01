428a325049e0b8d1b4d38e30999597a1f458d39a
Steven Toth <stoth@kernellabs.com>
2019-05-01 11:43:45 -0400

    Build system adjustments for additional packages

--- build.sh
+++ build.sh
@@ -6,14 +6,15 @@ LTNTSTOOLS_TAG=
 DEP_BITSTREAM_TAG=
 DEP_LIBDVBPSI_TAG=
 DEP_FFMPEG_TAG=
+LTNTSTOOLS_TAG=
 
 if [ "$1" == "" ]; then
 	# Fine if they do not specify a tag
 	echo "No specific tag specified.  Using master"
-	LTNTSTOOLS_TAG=master
 	DEP_BITSTREAM_TAG=20ce4345061499abc0389e9cd837665a62ad6add
 	DEP_LIBDVBPSI_TAG=d2a81c20a7704676048111b4f7ab24b95a904008
 	DEP_FFMPEG_TAG=9d3eb75cf637e6f2a664ad3ab67c4f785226f62e
+	LTNTSTOOLS_TAG=master
 elif [ "$1" == "--installdeps" ]; then
 	sudo yum -y install libpcap-devel
 	sudo yum -y install zlib-devel
@@ -21,10 +22,10 @@ elif [ "$1" == "--installdeps" ]; then
 	sudo yum -y install ncurses-devel
 	exit 0
 elif [ "$1" == "v1.0.1" ]; then
-	LTNTSTOOLS_TAG=v1.0.1
 	DEP_BITSTREAM_TAG=20ce4345061499abc0389e9cd837665a62ad6add
 	DEP_LIBDVBPSI_TAG=d2a81c20a7704676048111b4f7ab24b95a904008
 	DEP_FFMPEG_TAG=9d3eb75cf637e6f2a664ad3ab67c4f785226f62e
+	LTNTSTOOLS_TAG=v1.0.1
 else
 	echo "Invalid argument"
 	exit 1
@@ -51,24 +52,40 @@ if [ ! -d ffmpeg ]; then
 	fi
 fi
 
-#pushd bitstream
-#	make PREFIX=$PWD/../target-root/usr
-#	make PREFIX=$PWD/../target-root/usr install
-#popd
+if [ ! -d ltntstools ]; then
+	git clone git@github.com:LTN-Global/ltntstools.git
+	if [ "$LTNTSTOOLS_TAG" != "" ]; then
+		cd ltntstools && git checkout $LTNTSTOOLS_TAG && cd ..
+	fi
+fi
 
-#pushd libdvbpsi
-#	export CFLAGS="-I$PWD/../target-root/usr/include"
-#	export LDFLAGS="-L$PWD/../target-root/usr/lib"
-#	./bootstrap
-#	./configure --prefix=$PWD/../target-root/usr --enable-shared=no
-#	make -j$JOBS
-#	make install
-#popd
+pushd bitstream
+	make PREFIX=$PWD/../target-root/usr
+	make PREFIX=$PWD/../target-root/usr install
+popd
+
+pushd libdvbpsi
+	export CFLAGS="-I$PWD/../target-root/usr/include"
+	export LDFLAGS="-L$PWD/../target-root/usr/lib"
+	./bootstrap
+	./configure --prefix=$PWD/../target-root/usr --enable-shared=no
+	make -j$JOBS
+	make install
+popd
 
 pushd ffmpeg
 	export CFLAGS="-I$PWD/../target-root/usr/include"
 	export LDFLAGS="-L$PWD/../target-root/usr/lib"
-	#./configure --prefix=$PWD/../target-root/usr --disable-iconv --enable-static
+	./configure --prefix=$PWD/../target-root/usr --disable-iconv --enable-static
 	make -j$JOBS
-	#make install
+	make install
+popd
+
+pushd ltntstools
+	export CFLAGS="-I$PWD/../target-root/usr/include"
+	export LDFLAGS="-L$PWD/../target-root/usr/lib"
+	./autogen.sh --build
+	./configure --prefix=$PWD/../target-root/usr --enable-shared=no
+	make -j$JOBS
+	make install
 popd
